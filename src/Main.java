82aec5f2c9c04ccce0cf04b170078b8cdf775274
Maxime <maxime.latge@gmail.com>
2018-09-26 12:15:56 +0200

    ajouter ok sauf pour agrandir le tas

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
+import heap.SortedHeap;
+
 public class Main {
+
+
 
     public static void main(String[] args) {
 
-        System.out.println(7%2);
-        System.out.println(8%2);
+        SortedHeap test = new SortedHeap(11);
+
+        for(int i=0;i<=10;i++) {
+            test.insertElement(100+i);
+        }
+        boolean a = test.insertElement(3);
+
+        System.out.println(a);
+        System.out.println(test);
     }
 }
--- src/heap/SortedHeap.java
+++ src/heap/SortedHeap.java
@@ -8,15 +8,24 @@ public class SortedHeap implements Heap<Integer> {
     private Integer queue[];
 
     public SortedHeap(int capacity){
+        /* Constructeur */
         this.capacity=capacity;
         this.size=0;
         this.queue = new Integer[this.capacity];
 
     }
     public int posPere(int posFils){
-        /*retourne la position du père*/
-        if(posFils%2==1){return (posFils-1)/2;}
-        else{return (posFils-2)/2;}
+        /*retourne la position du père; si le fils est la racine du heap alors retourne 0*/
+        if (posFils>0) {
+            if (posFils % 2 == 1) {
+                return (posFils - 1) / 2;
+            } else {
+                return (posFils - 2) / 2;
+            }
+        }
+        else{
+            return 0;
+        }
     }
     public int[] posFils(int posPere){
         /*retourne la position des 2 fils*/
@@ -26,7 +35,39 @@ public class SortedHeap implements Heap<Integer> {
     }
 
     public boolean insertElement(Integer e){
-        return true;
+        /*ajoute un elt dans le heap et retourne true si l'ajout s'est bien passé, false sinon. */
+        try {
+
+            if (this.size > this.capacity) {
+                /*cas ou il n'y a pas de place pour ajouter notre élément */
+                this.capacity=capacity*2;
+                Integer newqueue[] = new Integer[capacity];
+                newqueue = queue.clone();
+                this.queue = newqueue;
+
+            }
+
+            /* procédure pour ajouter notre élément */
+            int pos = size;
+            queue[pos] = e;
+
+            int posp = posPere(pos);
+            int temp = queue[posp];
+            while (e > temp) {
+                queue[pos] = temp;
+                queue[posp] = e;
+                pos = posp;
+                posp = posPere(pos);
+                temp = queue[posp];
+            }
+            size++;
+
+
+        }catch(Exception pb){
+            return false;
+        }finally{
+            return true;
+        }
 
     }
 
@@ -45,6 +86,15 @@ public class SortedHeap implements Heap<Integer> {
 
     public int size(){
         return 0;
+    }
+
+    public String toString(){
+        /* permet d'afficher tous les éléments du heap */
+        String str ="";
+        for(int i=0;i<this.capacity;i++){
+            str+="position "+i+" : "+this.queue[i]+"\n";
+        }
+        return str;
     }
 
 }
